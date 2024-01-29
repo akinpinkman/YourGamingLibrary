d842c9624426f554e7ee02993aaec78b33d6bc6c
akinpinkman <akinbilgin54@hotmail.com>
2024-01-29 15:36:34 +0300

    Hide credentials

--- src/services/apiRecentPopularGames.js
+++ src/services/apiRecentPopularGames.js
@@ -1,11 +1,13 @@
-// apiRecentPopularGames.js
+const clientId = process.env.REACT_APP_CLIENT_ID
+const authorizationToken = process.env.REACT_APP_AUTHORIZATION_TOKEN
+
 export default async function getRecentPopularGames() {
   const response = await fetch('/api/v4/games', {
     method: 'POST',
     headers: {
       Accept: 'application/json',
-      'Client-ID': 'l2v8dn6vqb3cwqupu7ayri3ytu8f6j',
-      Authorization: 'Bearer 04ucijxaomxdus4npa6d1zu0afrhk6',
+      'Client-ID': clientId,
+      Authorization: `Bearer ${authorizationToken}`,
       'Content-Type': 'application/json'
     },
     body: 'fields name,first_release_date,rating,release_dates.human,cover.*,summary; where first_release_date >= 1672524061 & rating >= 60 & rating_count >= 20; limit 100;'
