3f6ddc775a77bf1c9f8f2662d948525b23296e09
akinpinkman <akinbilgin54@hotmail.com>
2024-01-29 16:13:41 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,23 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import eslint from 'vite-plugin-eslint';
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import eslint from 'vite-plugin-eslint'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
       '/api': {
         target: 'https://api.igdb.com',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
   },
-  plugins: [react(), eslint()]
-});
+  plugins: [react(), eslint()],
+  define: {
+    'process.env.VITE_REACT_APP_CLIENT_ID': JSON.stringify(process.env.VITE_REACT_APP_CLIENT_ID),
+    'process.env.VITE_REACT_APP_AUTHORIZATION_TOKEN': JSON.stringify(
+      process.env.VITE_REACT_APP_AUTHORIZATION_TOKEN
+    )
+  }
+})
