d365ea4fe28509eebda278e9c56bcccf84f43ac7
akinpinkman <akinbilgin54@hotmail.com>
2024-02-02 22:35:47 +0300

    Search Bar improved and Game Cards reusable

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import PageNotFound from './pages/PageNotFound'
 import AppLayout from './ui/AppLayout'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { ChakraProvider } from '@chakra-ui/react'
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -18,21 +19,21 @@ const queryClient = new QueryClient({
 export default function App() {
   return (
     // NOTE: GlobalStyles ekle.
-
-    <QueryClientProvider client={queryClient}>
-      <ReactQueryDevtools initialIsOpen={false} />
-      <BrowserRouter>
-        <Routes>
-          <Route element={<AppLayout />}>
-            <Route index element={<Navigate replace to="homepage" />} />
-            <Route path="homepage" element={<Homepage />} />
-            <Route path="gamelibrary" element={<GameLibrary />} />
-            <Route path="playlater" element={<PlayLater />} />
-          </Route>
-
-          <Route path="*" element={<PageNotFound />}></Route>
-        </Routes>
-      </BrowserRouter>
-    </QueryClientProvider>
+    <ChakraProvider>
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} />
+        <BrowserRouter>
+          <Routes>
+            <Route element={<AppLayout />}>
+              <Route index element={<Navigate replace to="homepage" />} />
+              <Route path="homepage" element={<Homepage />} />
+              <Route path="gamelibrary" element={<GameLibrary />} />
+              <Route path="playlater" element={<PlayLater />} />
+            </Route>
+            <Route path="*" element={<PageNotFound />}></Route>
+          </Routes>
+        </BrowserRouter>
+      </QueryClientProvider>
+    </ChakraProvider>
   )
 }
---
+++ src/components/GameCard/GameCardItemContainer.jsx
@@ -0,0 +1,41 @@
+import GameCardItem from './GameCardItem'
+import { usePopularGames } from '../../hooks/usePopularGames'
+import { useSearchGames } from '../../hooks/useSearchGames'
+import { useState } from 'react'
+import { Center, Spinner } from '@chakra-ui/react'
+
+export default function GameCardItemContainer() {
+  const [query, setQuery] = useState('')
+  const { isLoading, popularGames } = usePopularGames()
+  const { searchedGames } = useSearchGames(query)
+
+  if (isLoading) {
+    return (
+      <Spinner thickness="6px" speed="0.80s" emptyColor="gray.200" color="blue.500" size="xl" />
+    )
+  }
+
+  if (!popularGames) {
+    return null
+  }
+
+  return (
+    <div className="container mt-4">
+      <div className="row">
+        <form className="form-inline">
+          <input
+            className="form-control mr-sm-2"
+            onChange={(e) => setQuery(e.target.value)}
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+          />
+        </form>
+        {query === ''
+          ? popularGames.map((game, index) => <GameCardItem key={index} game={game} />)
+          : searchedGames &&
+            searchedGames.map((game, index) => <GameCardItem key={index} game={game} />)}
+      </div>
+    </div>
+  )
+}
